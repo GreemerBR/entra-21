aadb28ba8a46a4e0c1f475cdce0cb0bab460cbd1
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-05-13 21:10:22 -0300

    Novo projeto Foi criado um novo projeto para conter os exemplos do FOR.

---
+++ Entra21-ExemplosFor/Exemplo01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExemplosFor
+{
+    internal class Exemplo01
+    {
+        public void Executar()
+        {
+            for (int i = 0; i <= 23; i++)
+            {
+                for (int j = 0; j < 60; j++)
+                {
+                    for (int k = 0; k <= 59; k++)
+                    {
+                        Console.WriteLine(i + ":" + j + ":" + k);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Entra21-ExemplosFor/Program.cs
@@ -0,0 +1,17 @@
+﻿using Entra21.ExemplosFor;
+
+Console.WriteLine(@"-------------MENU-------------
+1 - Exemplo01
+2 - Exemplo02
+3 - Exemplo03
+4 - Exemplo04
+5 - Exemplo05");
+
+Console.Write("Digite a opção deseja: ");
+int opcaoDeseja = Convert.ToInt32(Console.ReadLine());
+
+if (opcaoDeseja == 1)
+{
+    Exemplo01 exemplo01 = new Exemplo01();
+    exemplo01.Executar();
+}
