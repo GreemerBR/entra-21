853fb39663a1521996cfa237b4ced63f12751dc2
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-05-13 21:54:43 -0300

    Novo projeto Foi feito o novo projeto referente aos exercícios para praticar FOR com TRY e CATCH. Iniciou-se o Porgram.cs e o Exercicio01, mas este último está com problemas. Verificar os Try Catch

---
+++ Entra21.ExerciciosForComTryCatch/Exercicio01.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosForComTryCatch
+{
+    internal class Exercicio01
+    {
+        // Solicite o preço e nome da peça de 13 peças.
+        public void Executar()
+        {
+            Console.Clear();
+
+            var nomePeca = "";
+            var precoPeca = 0.0;
+
+            for (var i = 0; i < 13; i++)
+            {
+                while (nomePeca.Length < 2)
+                {
+                    try
+                    {
+                        Console.Write("Informe o nome da peça: ");
+                        nomePeca = Console.ReadLine();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("O nome da peça informado não é válido. Este deve ter ao menos dois caracteres.");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+
+                    nomePeca = "";
+                }
+                
+                while (precoPeca <= 0.0)
+                {
+                    try
+                    {
+                        Console.Write("Informe o preço da peça: R$ ");
+                        precoPeca = Convert.ToDouble(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("O valor da peça informado não é válido. Este deve ser maior do que R$ 0,00.");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+
+                    precoPeca = 0.0;
+                }
+                
+            }
+        }
+    }
+}
---
+++ Entra21.ExerciciosForComTryCatch/Program.cs
@@ -0,0 +1,43 @@
+﻿using Entra21.ExerciciosForComTryCatch;
+
+var opcaoDesejada = 0;
+
+Console.Clear();    
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(@"-------------MENU-------------
+  1 - Exercício 01
+  5 - Exercício 05
+  8 - Exercício 08
+ 10 - Exercício 10
+ 12 - Exercício 12
+ 13 - Exercício 13");
+
+while (opcaoDesejada <= 0)
+{
+    try
+    {
+        Console.Write("Digite a opção desejada: ");
+        opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+
+        if (opcaoDesejada < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("A opção informada não é válida, por favor tente novamente!");
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("A opção informada não é válida, por favor tente novamente!");
+        Console.ForegroundColor = ConsoleColor.Green;
+    }
+
+    if (opcaoDesejada == 1)
+    {
+        Exercicio01 exemplo01 = new Exercicio01();
+        exemplo01.Executar();
+    }
+
+}
