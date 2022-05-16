88d9482d9c936438586c56fdb2bd040e662b95a4
Gregory Viegas Zimmer <83144131+GreemerBR@users.noreply.github.com>
2022-05-16 10:12:46 -0300

    Finalização exercicio01 Exercício 01 foi finalizado e testado. Aparentemente está funcionando.

--- Entra21.ExerciciosForComTryCatch/Exercicio01.cs
+++ Entra21.ExerciciosForComTryCatch/Exercicio01.cs
@@ -13,45 +13,56 @@ namespace Entra21.ExerciciosForComTryCatch
         {
             Console.Clear();
 
-            var nomePeca = "";
-            var precoPeca = 0.0;
+            var nomeTeste = false;
+            var precoTeste = false;
 
             for (var i = 0; i < 13; i++)
             {
-                while (nomePeca.Length < 2)
+                while (nomeTeste == false)
                 {
-                    try
+                    Console.Write("Informe o nome da peça: ");                        
+                    var nomePeca = Console.ReadLine();
+
+                    if (nomePeca.Length >= 2)
                     {
-                        Console.Write("Informe o nome da peça: ");
-                        nomePeca = Console.ReadLine();
+                        nomeTeste = true;
                     }
-                    catch (Exception ex)
+                    else
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("O nome da peça informado não é válido. Este deve ter ao menos dois caracteres.");
                         Console.ForegroundColor = ConsoleColor.Green;
                     }
-
-                    nomePeca = "";
                 }
-                
-                while (precoPeca <= 0.0)
+
+                while (precoTeste == false)
                 {
                     try
                     {
                         Console.Write("Informe o preço da peça: R$ ");
-                        precoPeca = Convert.ToDouble(Console.ReadLine());
+                        var precoPeca = Convert.ToDouble(Console.ReadLine());
+
+                        if (precoPeca > 0.0)
+                        {
+                            precoTeste = true;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("O valor da peça informado não é válido. Este deve ser maior do que R$ 0,00.");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
                     }
                     catch (Exception ex)
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("O valor da peça informado não é válido. Este deve ser maior do que R$ 0,00.");
+                        Console.WriteLine("O valor da peça informado não é válido. Este deve ser um número maior do que R$ 0,00.");
                         Console.ForegroundColor = ConsoleColor.Green;
                     }
-
-                    precoPeca = 0.0;
                 }
-                
+
+                nomeTeste = false;
+                precoTeste = false;
             }
         }
     }
