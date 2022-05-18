cfbbd5d044d7a951318744e0eb18c0c3796e818f
Gregory Viegas Zimmer <83144131+GreemerBR@users.noreply.github.com>
2022-05-18 10:53:19 -0300

    Alteração Program Foi alterado de modo a entrar em loop até o usuário informar que deseja sair.

--- Entra21.ExemplosWhile/Program.cs
+++ Entra21.ExemplosWhile/Program.cs
@@ -1,50 +1,80 @@
 ﻿using Entra21.ExemplosWhile;
 
-Console.WriteLine(@"-------------MENU-------------
+int opcaoDesejada = 0;
+
+while (opcaoDesejada != 6)
+{
+
+    Console.WriteLine(@"
+-------------MENU-------------
 1 - Exemplo01
 2 - Exemplo02
 3 - Exemplo03
 4 - Exemplo04
 5 - Exemplo05
 6 - Exemplo06
-7 - Exemplo07");
+7 - Exemplo07
+8 - SAIR
+");
 
-Console.Write("Digite a opção deseja: ");
-int opcaoDeseja = Convert.ToInt32(Console.ReadLine());
+    try
+    {
+        Console.Write("Digite a opção desejada: ");
+        opcaoDesejada = Convert.ToInt32(Console.ReadLine());
 
-if (opcaoDeseja == 1)
-{
-    //NomeDaClasse nomeVariavel = new NomeDaClasse
-    Exemplo01 exemplo01 = new Exemplo01();
-    exemplo01.Executar();
-}
-else if (opcaoDeseja == 2)
-{
-    Exemplo02 exemplo02 = new Exemplo02();
-    exemplo02.Executar();
-}
-else if (opcaoDeseja == 3)
-{
-    Exemplo03 exemplo03 = new Exemplo03();
-    exemplo03.Executar();
-}
-else if (opcaoDeseja == 4)
-{
-    Exemplo04 exemplo04 = new Exemplo04();
-    exemplo04.Executar();
-}
-else if (opcaoDeseja == 5)
-{
-    Exemplo05 exemplo05 = new Exemplo05();
-    exemplo05.Executar();
-}
-else if (opcaoDeseja == 6)
-{
-    Exemplo06 exemplo06 = new Exemplo06();
-    exemplo06.Executar();
-}
-else if (opcaoDeseja == 7)
-{
-    Exemplo07 exemplo07 = new Exemplo07();
-    exemplo07.Executar();
+        if (opcaoDesejada < 0 || (opcaoDesejada != 1 && opcaoDesejada != 2 && opcaoDesejada != 3 && opcaoDesejada != 4
+    && opcaoDesejada != 5 && opcaoDesejada != 6 && opcaoDesejada != 7 && opcaoDesejada != 8))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("A opção informada não é válida. Por favor informe um número presente no MENU.");
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        else
+        {
+            Console.Clear();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("A opção informada não é válida. Por favor informe um número presente no MENU.");
+        Console.ForegroundColor = ConsoleColor.Green;
+    }
+
+    if (opcaoDesejada == 1)
+    {
+        //NomeDaClasse nomeVariavel = new NomeDaClasse
+        Exemplo01 exemplo01 = new Exemplo01();
+        exemplo01.Executar();
+    }
+    else if (opcaoDesejada == 2)
+    {
+        Exemplo02 exemplo02 = new Exemplo02();
+        exemplo02.Executar();
+    }
+    else if (opcaoDesejada == 3)
+    {
+        Exemplo03 exemplo03 = new Exemplo03();
+        exemplo03.Executar();
+    }
+    else if (opcaoDesejada == 4)
+    {
+        Exemplo04 exemplo04 = new Exemplo04();
+        exemplo04.Executar();
+    }
+    else if (opcaoDesejada == 5)
+    {
+        Exemplo05 exemplo05 = new Exemplo05();
+        exemplo05.Executar();
+    }
+    else if (opcaoDesejada == 6)
+    {
+        Exemplo06 exemplo06 = new Exemplo06();
+        exemplo06.Executar();
+    }
+    else if (opcaoDesejada == 7)
+    {
+        Exemplo07 exemplo07 = new Exemplo07();
+        exemplo07.Executar();
+    }
 }
