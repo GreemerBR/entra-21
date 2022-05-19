57ff6db4cfad692182baf50370867e7ed388b253
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-05-18 21:14:29 -0300

    Exercicio16 Foi finalizado e testado o exercício 16. Também foi alterado o Program de modo que ele funcione em loop e só encerre no momento em que o usuário solicita a opção desejada.

--- Entra21.ExerciciosWhile/Exercicio16.cs
+++ Entra21.ExerciciosWhile/Exercicio16.cs
@@ -14,12 +14,173 @@ namespace Entra21.ExerciciosWhile
             Console.Write("Informe um texto: ");
             string texto = Console.ReadLine();
 
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            int e = 0;
+            int f = 0;
+            int g = 0;
+            int h = 0;
+            int i = 0;
+            int j = 0;
+            int k = 0;
+            int l = 0;
+            int m = 0;
+            int n = 0;
+            int o = 0;
+            int p = 0;
+            int q = 0;
+            int r = 0;
+            int s = 0;
+            int t = 0;
+            int u = 0;
+            int v = 0;
+            int w = 0;
+            int x = 0;
+            int y = 0;
+            int z = 0;
+
             int contador = 0;
 
             while (contador < texto.Length)
             {
+                char caracter = Convert.ToChar(texto.ToLower().Substring(contador, 1));
 
+                if (caracter == 'a')
+                {
+                    a = a + 1;
+                }
+                else if (caracter == 'b')
+                {
+                    b = b + 1;
+                }
+                else if (caracter == 'c')
+                {
+                    c = c + 1;
+                }
+                else if (caracter == 'd')
+                {
+                    d = d + 1;
+                }
+                else if (caracter == 'e')
+                {
+                    e = e + 1;
+                }
+                else if (caracter == 'f')
+                {
+                    f = f + 1;
+                }
+                else if (caracter == 'g')
+                {
+                    g = g + 1;
+                }
+                else if (caracter == 'h')
+                {
+                    h = h + 1;
+                }
+                else if (caracter == 'i')
+                {
+                    i = i + 1;
+                }
+                else if (caracter == 'j')
+                {
+                    j = j + 1;
+                }
+                else if (caracter == 'k')
+                {
+                    k = k + 1;
+                }
+                else if (caracter == 'l')
+                {
+                    l = l + 1;
+                }
+                else if (caracter == 'm')
+                {
+                    m = m + 1;
+                }
+                else if (caracter == 'n')
+                {
+                    n = n + 1;
+                }
+                else if (caracter == 'o')
+                {
+                    o = o + 1;
+                }
+                else if (caracter == 'p')
+                {
+                    p = p + 1;
+                }
+                else if (caracter == 'q')
+                {
+                    q = q + 1;
+                }
+                else if (caracter == 'r')
+                {
+                    r = r + 1;
+                }
+                else if (caracter == 's')
+                {
+                    s = s + 1;
+                }
+                else if (caracter == 't')
+                {
+                    t = t + 1;
+                }
+                else if (caracter == 'u')
+                {
+                    u = u + 1;
+                }
+                else if (caracter == 'v')
+                {
+                    v = v + 1;
+                }
+                else if (caracter == 'w')
+                {
+                    w = w + 1;
+                }
+                else if (caracter == 'x')
+                {
+                    x = x + 1;
+                }
+                else if (caracter == 'y')
+                {
+                    y = y + 1;
+                }
+                else if (caracter == 'z')
+                {
+                    z = z + 1;
+                }
+
+                contador = contador + 1;
             }
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+            Console.WriteLine("e = " + e);
+            Console.WriteLine("f = " + f);
+            Console.WriteLine("g = " + g);
+            Console.WriteLine("h = " + h);
+            Console.WriteLine("i = " + i);
+            Console.WriteLine("j = " + j);
+            Console.WriteLine("k = " + k);
+            Console.WriteLine("l = " + l);
+            Console.WriteLine("m = " + m);
+            Console.WriteLine("n = " + n);
+            Console.WriteLine("o = " + o);
+            Console.WriteLine("p = " + p);
+            Console.WriteLine("q = " + q);
+            Console.WriteLine("r = " + r);
+            Console.WriteLine("s = " + s);
+            Console.WriteLine("t = " + t);
+            Console.WriteLine("u = " + u);
+            Console.WriteLine("v = " + v);
+            Console.WriteLine("w = " + w);
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("z = " + z);
         }
 
     }
--- Entra21.ExerciciosWhile/Program.cs
+++ Entra21.ExerciciosWhile/Program.cs
@@ -1,6 +1,11 @@
 ﻿using Entra21.ExerciciosWhile;
 
-Console.WriteLine(@"-------------MENU-------------
+int opcaoDesejada = 0;
+
+while (opcaoDesejada != 18)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(@"-------------MENU-------------
  1 - Exercício 01
  2 - Exercício 02
  3 - Exercício 03
@@ -18,94 +23,118 @@ Console.WriteLine(@"-------------MENU-------------
 15 - Exercício 15
 16 - Exercício 16
 17 - Exercício 17
+18 - SAIR
 ");
 
-Console.Write("Digite a opção deseja: ");
-int opcaoDeseja = Convert.ToInt32(Console.ReadLine());
-Console.Clear();
+    try
+    {
+        Console.Write("Digite a opção desejada: ");
+        opcaoDesejada = Convert.ToInt32(Console.ReadLine());
 
-if (opcaoDeseja == 1)
-{
-    Exercicio01 Exercicio01 = new Exercicio01();
-    Exercicio01.Executar();
-}
-else if (opcaoDeseja == 2)
-{
-    Exercicio02 Exercicio02 = new Exercicio02();
-    Exercicio02.Executar();
-}
-else if (opcaoDeseja == 3)
-{
-    Exercicio03 Exercicio03 = new Exercicio03();
-    Exercicio03.Executar();
-}
-else if (opcaoDeseja == 4)
-{
-    Exercicio04 Exercicio04 = new Exercicio04();
-    Exercicio04.Executar();
-}
-else if (opcaoDeseja == 5)
-{
-    Exercicio05 Exercicio05 = new Exercicio05();
-    Exercicio05.Executar();
-}
-else if (opcaoDeseja == 6)
-{
-    Exercicio06 Exercicio06 = new Exercicio06();
-    Exercicio06.Executar();
-}
-else if (opcaoDeseja == 7)
-{
-    Exercicio07 Exercicio07 = new Exercicio07();
-    Exercicio07.Executar();
-}
-else if (opcaoDeseja == 8)
-{
-    Exercicio08 Exercicio08 = new Exercicio08();
-    Exercicio08.Executar();
-}
-else if (opcaoDeseja == 9)
-{
-    Exercicio09 Exercicio09 = new Exercicio09();
-    Exercicio09.Executar();
-}
-else if (opcaoDeseja == 10)
-{
-    Exercicio10 Exercicio10 = new Exercicio10();
-    Exercicio10.Executar();
-}
-else if (opcaoDeseja == 11)
-{
-    Exercicio11 Exercicio11 = new Exercicio11();
-    Exercicio11.Executar();
-}
-else if (opcaoDeseja == 12)
-{
-    Exercicio12 Exercicio12 = new Exercicio12();
-    Exercicio12.Executar();
-}
-else if (opcaoDeseja == 13)
-{
-    Exercicio13 Exercicio13 = new Exercicio13();
-    Exercicio13.Executar();
-}
-else if (opcaoDeseja == 14)
-{
-    Exercicio14 Exercicio14 = new Exercicio14();
-    Exercicio14.Executar();
-}
-else if (opcaoDeseja == 15)
-{
-    Exercicio15 Exercicio15 = new Exercicio15();
-    Exercicio15.Executar();
-}
-else if (opcaoDeseja == 16)
-{
-    Exercicio16 Exercicio16 = new Exercicio16();
-    Exercicio16.Executar();
-}
-else if (opcaoDeseja == 17)
-{
-    Exercicio17 Exercicio17 = new Exercicio17();
-    Exercicio17.Executar();
+        if (opcaoDesejada < 0 || (opcaoDesejada != 1 && opcaoDesejada != 2 && opcaoDesejada != 3 && opcaoDesejada != 4 && opcaoDesejada != 5 && opcaoDesejada != 6
+            && opcaoDesejada != 7 && opcaoDesejada != 8 && opcaoDesejada != 9 && opcaoDesejada != 10 && opcaoDesejada != 11 && opcaoDesejada != 12 && opcaoDesejada != 13
+            && opcaoDesejada != 14 && opcaoDesejada != 15 && opcaoDesejada != 16 && opcaoDesejada != 17 && opcaoDesejada != 18)) 
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("A opção informada não é válida. Por favor informe um número presente no MENU.");
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        else
+        {
+            Console.Clear();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("A opção informada não é válida. Por favor informe um número presente no MENU.");
+        Console.ForegroundColor = ConsoleColor.Green;
+    }
+
+
+    if (opcaoDesejada == 1)
+    {
+        Exercicio01 Exercicio01 = new Exercicio01();
+        Exercicio01.Executar();
+    }
+    else if (opcaoDesejada == 2)
+    {
+        Exercicio02 Exercicio02 = new Exercicio02();
+        Exercicio02.Executar();
+    }
+    else if (opcaoDesejada == 3)
+    {
+        Exercicio03 Exercicio03 = new Exercicio03();
+        Exercicio03.Executar();
+    }
+    else if (opcaoDesejada == 4)
+    {
+        Exercicio04 Exercicio04 = new Exercicio04();
+        Exercicio04.Executar();
+    }
+    else if (opcaoDesejada == 5)
+    {
+        Exercicio05 Exercicio05 = new Exercicio05();
+        Exercicio05.Executar();
+    }
+    else if (opcaoDesejada == 6)
+    {
+        Exercicio06 Exercicio06 = new Exercicio06();
+        Exercicio06.Executar();
+    }
+    else if (opcaoDesejada == 7)
+    {
+        Exercicio07 Exercicio07 = new Exercicio07();
+        Exercicio07.Executar();
+    }
+    else if (opcaoDesejada == 8)
+    {
+        Exercicio08 Exercicio08 = new Exercicio08();
+        Exercicio08.Executar();
+    }
+    else if (opcaoDesejada == 9)
+    {
+        Exercicio09 Exercicio09 = new Exercicio09();
+        Exercicio09.Executar();
+    }
+    else if (opcaoDesejada == 10)
+    {
+        Exercicio10 Exercicio10 = new Exercicio10();
+        Exercicio10.Executar();
+    }
+    else if (opcaoDesejada == 11)
+    {
+        Exercicio11 Exercicio11 = new Exercicio11();
+        Exercicio11.Executar();
+    }
+    else if (opcaoDesejada == 12)
+    {
+        Exercicio12 Exercicio12 = new Exercicio12();
+        Exercicio12.Executar();
+    }
+    else if (opcaoDesejada == 13)
+    {
+        Exercicio13 Exercicio13 = new Exercicio13();
+        Exercicio13.Executar();
+    }
+    else if (opcaoDesejada == 14)
+    {
+        Exercicio14 Exercicio14 = new Exercicio14();
+        Exercicio14.Executar();
+    }
+    else if (opcaoDesejada == 15)
+    {
+        Exercicio15 Exercicio15 = new Exercicio15();
+        Exercicio15.Executar();
+    }
+    else if (opcaoDesejada == 16)
+    {
+        Exercicio16 Exercicio16 = new Exercicio16();
+        Exercicio16.Executar();
+    }
+    else if (opcaoDesejada == 17)
+    {
+        Exercicio17 Exercicio17 = new Exercicio17();
+        Exercicio17.Executar();
+    }
 }
