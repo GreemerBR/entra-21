cdf87d3490becd6ab16f1662b2e08be263aaa3b7
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-05-20 18:38:05 -0300

    Exercicio02 Foi iniciado, finalizado e testado o exercício 02. Também foi alterado o Program para executar o exercício 02 e para que durante o loop apresente o menu sempre que o usuário informar uma opção que não seja valida.

---
+++ Entra21.ExerciciosVetor/Exercicio02.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosVetor
+{
+    internal class Exercicio02
+    {
+        //Crie um vetor para guardar 10 nomes e apresente os dez nomes
+        public void Executar()
+        {
+            var nomes = new string[10];
+
+            for (var i = 0; i < nomes.Length; i++)
+            {
+                var nome = "";
+                var teste = false;
+
+                while (teste == false)
+                {
+                    Console.Clear();
+                    Console.Write("Informe o nome: ");
+                    nome = Console.ReadLine().Trim();
+
+                    if (nome.Length < 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("O nome informado não é valido. Por favor informe um nome com no mínimo 3 caracteres.");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        teste = true;
+                    }
+                }
+
+                nomes[i] = nome;
+            }
+
+            var listaNomes = "";
+
+            for (var i =0; i < nomes.Length; i++)
+            {
+                listaNomes = listaNomes + nomes[i] + ", ";
+            }
+
+            Console.Clear();
+            Console.WriteLine(listaNomes);
+        }
+    }
+}
--- Entra21.ExerciciosVetor/Program.cs
+++ Entra21.ExerciciosVetor/Program.cs
@@ -1,9 +1,10 @@
 ﻿using Entra21.ExerciciosVetor;
 
 Console.ForegroundColor = ConsoleColor.Green;
-
 var opcaoDesejada = 0;
-while (opcaoDesejada != 8)
+var teste = false;
+
+while (teste == false)
 {
     Console.Write(@"
 ------ MENU ------
@@ -19,36 +20,38 @@ while (opcaoDesejada != 8)
 
 Informe a opção desejada: ");
 
-    var teste = false;
-    while (teste == false)
+    try
     {
-        try
-        {
-            opcaoDesejada = Convert.ToInt32(Console.ReadLine().Trim());
+        opcaoDesejada = Convert.ToInt32(Console.ReadLine().Trim());
 
-            if (opcaoDesejada < 0 || (opcaoDesejada > 0 && (opcaoDesejada != 1 && opcaoDesejada != 2 && opcaoDesejada != 3 && opcaoDesejada != 4
-                && opcaoDesejada != 5 && opcaoDesejada != 6 && opcaoDesejada != 7 && opcaoDesejada != 8)))
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("A opção informada não é valida. Por favor informe um número presente no MENU.");
-                Console.ForegroundColor = ConsoleColor.Green;
-            }
-            else
-            {
-                teste = true;
-            }
-        }
-        catch (Exception ex)
+        if (opcaoDesejada < 0 || (opcaoDesejada > 0 && (opcaoDesejada != 1 && opcaoDesejada != 2 && opcaoDesejada != 3 && opcaoDesejada != 4
+            && opcaoDesejada != 5 && opcaoDesejada != 6 && opcaoDesejada != 7 && opcaoDesejada != 8)))
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("A opção informada não é valida. Por favor informe um número presente no MENU.");
             Console.ForegroundColor = ConsoleColor.Green;
         }
+        else
+        {
+            teste = true;
+        }
     }
-
-    if (opcaoDesejada == 1)
+    catch (Exception ex)
     {
-        var exercicio01 = new Exercicio01();
-        exercicio01.Executar();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("A opção informada não é valida. Por favor informe um número presente no MENU.");
+        Console.ForegroundColor = ConsoleColor.Green;
     }
+}
+
+if (opcaoDesejada == 1)
+{
+    var exercicio01 = new Exercicio01();
+    exercicio01.Executar();
+}
+
+if (opcaoDesejada == 2)
+{
+    var exercicio02 = new Exercicio02();
+    exercicio02.Executar();
 }
