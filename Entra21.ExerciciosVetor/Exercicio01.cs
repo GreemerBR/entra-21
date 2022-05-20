63ce94bd3a8a2df9915d0718de9711ab2002cc9e
Gregory Viegas Zimmer <83144131+GreemerBR@users.noreply.github.com>
2022-05-20 14:44:00 -0300

    Exercicio01 / Program Foi iniciado, finalizado e testado o exercício 01. Também foi iniciado o Porgram de modo a poder ser executado o Exercicio01. O Program se encontra em loop até que o usuário informe a opcão desejada para SAIR.

---
+++ Entra21.ExerciciosVetor/Exercicio01.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosVetor
+{
+    internal class Exercicio01
+    {
+        /* Crie um vetor para guardar 16 números inteiros.
+         * Preencha todas as posições com números e apresente a soma destes números.
+         */
+        public void Executar()
+        {
+            var numeros = new int[16];
+            var soma = 0;
+
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                var numero = 0;
+                var teste = false;
+
+                while(teste == false)
+                {
+                    try
+                    {
+                        Console.Clear();
+                        Console.Write("Informe um número inteiro: ");
+                        numero = Convert.ToInt32(Console.ReadLine().Trim());
+
+                        teste = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Valor informado não é valido. Por favor informe um número inteiro.");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                }
+
+                numeros[i] = numero;
+                soma = soma + numeros[i];
+            }
+
+            Console.Clear();
+            Console.WriteLine($"A soma dos números informados é {soma}");
+        }
+    }
+}
---
+++ Entra21.ExerciciosVetor/Program.cs
@@ -0,0 +1,54 @@
+﻿using Entra21.ExerciciosVetor;
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+var opcaoDesejada = 0;
+while (opcaoDesejada != 8)
+{
+    Console.Write(@"
+------ MENU ------
+1 - Exercício 01
+2 - Exercício 02
+3 - Exercício 03
+4 - Exercício 04
+5 - Exercício 05
+6 - Exercício 06
+7 - Exercício 07
+8 - SAIR
+
+
+Informe a opção desejada: ");
+
+    var teste = false;
+    while (teste == false)
+    {
+        try
+        {
+            opcaoDesejada = Convert.ToInt32(Console.ReadLine().Trim());
+
+            if (opcaoDesejada < 0 || (opcaoDesejada > 0 && (opcaoDesejada != 1 && opcaoDesejada != 2 && opcaoDesejada != 3 && opcaoDesejada != 4
+                && opcaoDesejada != 5 && opcaoDesejada != 6 && opcaoDesejada != 7 && opcaoDesejada != 8)))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("A opção informada não é valida. Por favor informe um número presente no MENU.");
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                teste = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("A opção informada não é valida. Por favor informe um número presente no MENU.");
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+    }
+
+    if (opcaoDesejada == 1)
+    {
+        var exercicio01 = new Exercicio01();
+        exercicio01.Executar();
+    }
+}
