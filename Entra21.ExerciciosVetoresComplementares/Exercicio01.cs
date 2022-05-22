173ed3255c4837a686ac2d8faca119f105d8cf66
Gregory Viegas Zimmer <83144131+GreemerBR@users.noreply.github.com>
2022-05-22 11:54:44 -0300

    Exercicio01 / Program Foi iniciado, finalizado e testado o exercício 01. Também foi feito o Program de modo a executar o exercício 01.

---
+++ Entra21.ExerciciosVetoresComplementares/Exercicio01.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosVetoresComplementares
+{
+    internal class Exercicio01
+    {
+        /* Criar um vetor para armazenar 10 números, solicite os números e armazene. Será aceito
+         * neste vetor somente números pares
+         */
+        public void Executar()
+        {
+            Console.Clear();
+            var numerosPares = new int[10];
+            
+            for (var i = 0; i < numerosPares.Length;)
+            {
+                var numero = 0;
+
+                while (numero <= 0)
+                {
+                    try
+                    {
+                        Console.Write("Informe um número: ");
+                        numero = Convert.ToInt32(Console.ReadLine());
+
+                        if (numero <= 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("O número informado é invalido. Por favor informe um número maior ou igual a zero.");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else if (numero % 2 == 0)
+                        {
+                            numerosPares[i] = numero;                            
+                            i++;
+                        }                       
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("O número informado é invalido. Por favor informe um número maior ou igual a zero.");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Os 10 números pares informados foram: ");
+            for (var i = 0; i < numerosPares.Length; i++)
+            {
+                Console.WriteLine(numerosPares[i]);
+            }
+        }
+    }
+}
---
+++ Entra21.ExerciciosVetoresComplementares/Program.cs
@@ -0,0 +1,85 @@
+﻿using Entra21.ExerciciosVetoresComplementares;
+
+Console.ForegroundColor = ConsoleColor.Green;
+var opcaoDesejada = 0;
+
+while (opcaoDesejada != 8)
+{
+    Console.Write(@"
+------ MENU ------
+1 - Exercício 01
+2 - Exercício 02
+3 - Exercício 03
+4 - Exercício 04
+5 - Exercício 05
+6 - Exercício 06
+7 - Exercício 07
+8 - SAIR
+
+Informe a opção desejada: ");
+
+    try
+    {
+        opcaoDesejada = Convert.ToInt32(Console.ReadLine().Trim());
+
+        if (opcaoDesejada < 0 || (opcaoDesejada > 0 && (opcaoDesejada != 1 && opcaoDesejada != 2 && opcaoDesejada != 3 && opcaoDesejada != 4
+            && opcaoDesejada != 5 && opcaoDesejada != 6 && opcaoDesejada != 7 && opcaoDesejada != 8)))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("A opção informada não é valida. Por favor informe um número presente no MENU.");
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        else
+        {
+            Console.Clear();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("A opção informada não é valida. Por favor informe um número presente no MENU.");
+        Console.ForegroundColor = ConsoleColor.Green;
+    }
+
+    if (opcaoDesejada == 1)
+    {
+        var exercicio01 = new Exercicio01();
+        exercicio01.Executar();
+    }
+
+    /* if (opcaoDesejada == 2)
+     {
+         var exercicio02 = new Exercicio02();
+         exercicio02.Executar();
+     }
+
+     if (opcaoDesejada == 3)
+     {
+         var exercicio03 = new Exercicio03();
+         exercicio03.Executar();
+     }
+
+     if (opcaoDesejada == 4)
+     {
+         var exercicio04 = new Exercicio04();
+         exercicio04.Executar();
+     }
+
+     if (opcaoDesejada == 5)
+     {
+         var exercicio05 = new Exercicio05();
+         exercicio05.Executar();
+     }
+
+     if (opcaoDesejada == 6)
+     {
+         var exercicio06 = new Exercicio06();
+         exercicio06.Executar();
+     }
+
+     if (opcaoDesejada == 7)
+     {
+         var exercicio07 = new Exercicio07();
+         exercicio07.Executar();
+     }*/
+}
