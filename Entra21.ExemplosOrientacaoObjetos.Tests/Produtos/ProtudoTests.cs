2fa86ef538e67bd895db2b2e88802bddb7449a2f
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-05-26 20:34:12 -0300

    Exemplos Orientação a Objetos Exemplos feitos em aula pelo professor

---
+++ Entra21.ExemplosOrientacaoObjetos.Tests/Produtos/ProtudoTests.cs
@@ -0,0 +1,86 @@
+﻿using Entra21.ExemplosOrientacaoObjetos.Produtos;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Entra21.ExemplosOrientacaoObjetos.Tests.Produtos
+{
+    public class ProtudoTests
+    {
+        [Fact]
+        public void Validar_EhVencido_Com_DataVencimento_Menor_Hoje()
+        {
+            // Arrange (preparar od dados)
+            var produto = new Produto();
+            produto.DataVencimento = DateTime.Today.AddDays(-1);
+
+            // Act (ato da execução do método que deseja testar)
+            var produtoVencido = produto.EhVencido();
+
+            // Assert (validação do que foi gerado)
+            produtoVencido.Should().Be(true);
+            // produtoVencido.Should().BeTrue(); -> outra forma do "Be(True)"
+        }
+
+        [Fact]
+        public void Validar_EhVencido_Com_DataVencimento_Maior_Hoje()
+        {
+            // Arrange
+            var produto = new Produto();
+            produto.DataVencimento = DateTime.Today.AddDays(1);
+
+            // Act
+            var produtoVencido = produto.EhVencido();
+
+            // Assert
+            produtoVencido.Should().Be(false);
+            //vencido.Should().BeFalse(); -> outra forma do "Be(false)"
+        }
+
+        [Fact]
+        public void Validar_EhVencido_Com_DataVencimento_Hoje()
+        {
+            // Arrange
+            var produto = new Produto();
+            produto.DataVencimento = DateTime.Today;
+
+            // Act
+            var vencido = produto.EhVencido();
+
+            // Assert
+            vencido.Should().Be(true);
+        }
+
+        [Fact]
+        public void Validar_EstaEmPromocao_Verdadeiro()
+        {
+            // Arrange
+            var produto = new Produto();
+            produto.DataVencimento = DateTime.Today.AddDays(3);
+
+            // Act
+            var emPromocao = produto.EstaEmPromocao();
+
+            // Assert
+            emPromocao.Should().Be(true);
+        }
+
+        [Fact]
+        public void Validar_EstaEmPromocao_Falso()
+        {
+            // Arrange
+            var produto = new Produto();
+            produto.DataVencimento = DateTime.Today;
+
+            // Act
+            var emPromocao = produto.EstaEmPromocao();
+
+            // Assert
+            emPromocao.Should().Be(false);
+        }
+    }
+}
---
+++ Entra21.ExemplosOrientacaoObjetos/Produtos/Produto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExemplosOrientacaoObjetos.Produtos
+{
+    public class Produto
+    {
+        public string Nome;
+        public DateTime DataFabricacao;
+        public DateTime DataVencimento;
+
+        public bool EhVencido()
+        {
+            var dataHoje = DateTime.Today;
+
+            if (DataVencimento > dataHoje)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool EstaEmPromocao()
+        {
+            var vencido = EhVencido();
+
+            if (vencido == true)
+            {
+                return false;
+            }
+
+            var dataHoje = DateTime.Today;
+
+            // Tirar 3 dias da data de vencimento
+            var dataInicioPromocao = DataVencimento.AddDays(-3);
+
+            var dataTerminoPromocao = DataVencimento.AddDays(-1);
+
+            if (dataHoje >= dataInicioPromocao && dataHoje <= dataTerminoPromocao)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
