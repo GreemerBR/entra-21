0f5b520dd5bd4ff7b42d9075711082f657dab193
Gregory Viegas Zimmer <83144131+GreemerBR@users.noreply.github.com>
2022-05-27 03:07:54 -0300

    ExercicioLivros Inicio do exercício Livros

---
+++ Entra21.ExercicioOrientacaoObjetos/Livros/Livro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExercicioOrientacaoObjetos.Livros
+{
+    public class Livro
+    {
+       
+
+        public string Titulo, Autor, IdiomaOriginal;
+        public int CodigoIsbn, QuantidadePaginas, QuantidadePaginasLidas, QuantidadeReleituras;
+        public DateTime DataLancamento;
+
+
+        
+    }
+}
