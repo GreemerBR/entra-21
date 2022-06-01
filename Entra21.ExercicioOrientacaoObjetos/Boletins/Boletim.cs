4bf2a3a9a15a37eeefaad96e98b4ce26293169d7
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-06-01 18:55:04 -0300

    Exercicio Boletim Iniciado o exercicio boletim

---
+++ Entra21.ExercicioOrientacaoObjetos/Boletins/Boletim.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExercicioOrientacaoObjetos.Boletins
+{
+    public class Boletim
+    {
+        public double[] NotasMatematica = new double[3];
+        public double[] NotasPortugues = new double[3];
+        public double[] NotasFisica = new double[3];
+        public double[] NotasQuimica = new double[3];
+        public double[] NotasGeografia = new double[3];
+        public double[] NotasHistoria = new double[3];
+        public double[] NotasFilosofia = new double[3];
+        public double[] NotasSociologia = new double[3];
+        public double[] NotasBiologia = new double[3];
+        public int QuantidadeDiasLetivos, QuantidadePresencas;
+
+        public double CalcularMediaMatematica()
+        {
+            var somaNotasMatematica = 0.0;
+
+            for (var i = 0; i < 3; i++)
+            {
+                somaNotasMatematica = somaNotasMatematica + NotasMatematica[i];
+            }
+
+            var mediaMatematica = somaNotasMatematica / 3;
+
+            return mediaMatematica;
+        }
+
+        public double CalcularMediaPortugues()
+        {
+            var somaNotasPortugues = 0.0;
+
+            for (var i = 0; i < 3; i++)
+            {
+                somaNotasPortugues = somaNotasPortugues + NotasPortugues[i];
+            }
+
+            var mediaPortugues = somaNotasPortugues / 3;
+
+            return mediaPortugues;
+        }
+
+        public double CalcularMediaFisica()
+        {
+            var somaNotasFisica = 0.0;
+
+            for (var i = 0; i < 3; i++)
+            {
+                somaNotasFisica = somaNotasFisica + NotasFisica[i];
+            }
+
+            var mediaFisica = somaNotasFisica / 3;
+
+            return mediaFisica;
+        }
+
+        public double CalcularMediaQuimica()
+        {
+            var somaNotasQuimica = 0.0;
+
+            for (var i = 0; i < 3; i++)
+            {
+                somaNotasQuimica = somaNotasQuimica + NotasQuimica[i];
+            }
+
+            var mediaQuimica = somaNotasQuimica / 3;
+
+            return mediaQuimica;
+        }
+
+        public double CalcularMediaGeografia()
+        {
+            var somaNotasGeografia = 0.0;
+
+            for (var i = 0; i < 3; i++)
+            {
+                somaNotasGeografia = somaNotasGeografia + NotasGeografia[i];
+            }
+
+            var mediaGeografia = somaNotasGeografia / 3;
+
+            return mediaGeografia;
+        }
+
+        public double CalcularMediaHistoria()
+        {
+            var somaNotasHistoria = 0.0;
+
+            for (var i = 0; i < 3; i++)
+            {
+                somaNotasHistoria = somaNotasHistoria + NotasHistoria[i];
+            }
+
+            var mediaHistoria = somaNotasHistoria / 3;
+
+            return mediaHistoria;
+        }
+
+        public double CalcularMediaFilosofia()
+        {
+            var somaNotasFilosofia = 0.0;
+
+            for (var i = 0; i < 3; i++)
+            {
+                somaNotasFilosofia = somaNotasFilosofia + NotasFilosofia[i];
+            }
+
+            var mediaFilosofia = somaNotasFilosofia / 3;
+
+            return mediaFilosofia;
+        }
+
+        public double CalcularMediaSociologia()
+        {
+            var somaNotasSociologia = 0.0;
+
+            for (var i = 0; i < 3; i++)
+            {
+                somaNotasSociologia = somaNotasSociologia + NotasSociologia[i];
+            }
+
+            var mediaSociologia = somaNotasSociologia / 3;
+
+            return mediaSociologia;
+        }
+
+        public double CalcularMediaBiologia()
+        {
+            var somaNotasBiologia = 0.0;
+
+            for (var i = 0; i < 3; i++)
+            {
+                somaNotasBiologia = somaNotasBiologia + NotasBiologia[i];
+            }
+
+            var mediaBiologia = somaNotasBiologia / 3;
+
+            return mediaBiologia;
+        }
+
+        public int CalcularQuantidadeDeFaltas()
+        {
+            var quantidadeDeFaltas = QuantidadeDiasLetivos - QuantidadePresencas;
+
+            return quantidadeDeFaltas;
+        }
+
+        public double CalcularPercentualDePresencas()
+        {
+            var percentualDePresencas = QuantidadePresencas * 100 / QuantidadeDiasLetivos;
+
+            return percentualDePresencas;
+        }
+
+        public double CalcularPercentualDeFaltas()
+        {
+            var quantidadeDeFaltas = CalcularQuantidadeDeFaltas();
+            var percentualDeFaltas = quantidadeDeFaltas * 100 / QuantidadeDiasLetivos;
+
+            return percentualDeFaltas;
+        }
+
+        public double CalcularMediaGeral()
+        {
+            var mediaMatematica = CalcularMediaMatematica();
+            var mediaPortugues = CalcularMediaPortugues();
+            var mediaFisica = CalcularMediaFisica();
+            var mediaQuimica = CalcularMediaQuimica();
+            var mediaGeografia = CalcularMediaMatematica();
+            var mediaHistoria = CalcularMediaHistoria();
+            var mediaFilosofia = CalcularMediaFilosofia();
+            var mediaSociologia = CalcularMediaSociologia();
+            var mediaBiologia = CalcularMediaBiologia();
+
+            var mediaGeral = (mediaMatematica + mediaPortugues + mediaFisica + mediaQuimica + mediaGeografia +
+                mediaHistoria + mediaFilosofia + mediaSociologia + mediaBiologia) / 9;
+
+            return mediaGeral;
+        }
+
+
+    }
+}
---
+++ Entra21.ExercicioOrientacaoObjetos/Boletins/ExercicioBoletim.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExercicioOrientacaoObjetos.Boletins
+{
+    public class ExercicioBoletim
+    {
+        /* Criar uma classe chamada Boletim armazenar:
+         *   ● Vetor para notas de matemática
+         *   ● Vetor para notas de português
+         *   ● Vetor para notas de física
+         *   ● Vetor para notas de química
+         *   ● Vetor para notas de geografia
+         *   ● Vetor para notas de história
+         *   ● Vetor para notas de filosofia
+         *   ● Vetor para notas de sociologia
+         *   ● Vetor para notas de biologia
+         *   ● Quantidade de dias letivos
+         *   ● Quantidade de presenças
+         *  Criar métodos:
+         *   ● Calcular média de matemática
+         *   ● Calcular média de português
+         *   ● ….
+         *   ● Calcular quantidade de faltas
+         *   ● Calcular percentual de presença
+         *   ● Calcular percentual de faltas
+         *   ● Apresentar boletim(n precisa de teste)
+         *   ● Validar em exame
+         *   ● Validar reprovado
+         *   ● Calcular média geral
+         *   ● Validar passou de ano
+         *  Criar testes unitários validando os métodos acima.
+         *  Criar classe ExemploBoletim
+         */
+    }
+}
