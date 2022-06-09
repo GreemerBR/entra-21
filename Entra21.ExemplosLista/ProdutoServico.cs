53575a98b37fb1d17b2b86844969ffc09c21c80b
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-06-09 19:45:18 -0300

    Continuação do Exemplo Produto Foi realizado a continuação do exemplo produto, sendo criado os métodos editar, apagar e obter por código. Também foi criado a variável código atual para poder crir o ID dos produtos.

--- Entra21.ExemplosLista/ProdutoServico.cs
+++ Entra21.ExemplosLista/ProdutoServico.cs
@@ -3,6 +3,9 @@
     internal class ProdutoServico
     {
         private List<Produto> produtos = new List<Produto>();
+
+        // Armazenar o código do próximo produto
+        private int codigoAtual = 1;
         
         // encapsulamento + tipoRetorno + NomeMetodo(parametros)
         public void Adicionar(string nome, double precoUnitario, ProdutoLocalizacao localizacao, int quantidade)
@@ -16,6 +19,12 @@
             produto.Localizacao = localizacao;
             produto.Quantidade = quantidade;
 
+            // Atribuir o código novo para o objeto do produto
+            produto.Codigo = codigoAtual;
+
+            // Incrementar 1 no código atual para quando criar um novo produto o produto contenha outra código
+            codigoAtual = codigoAtual + 1;
+
             // Adicionar o produto instanciado na lista de produtos
             produtos.Add(produto);
         }
@@ -23,16 +32,52 @@
         {
 
         }
-        public void Apagar()
+        public bool Apagar(int codigo)
         {
+            for (var i = 0; i < produtos.Count; i++)
+            {
+                // Resgatando de uma lista de tipo primitivo
+                // int numero = numeros[i];
 
+                // Resgatando de uma lista de objetos
+                Produto produto = produtos[i];
+
+                if (produto.Codigo == codigo)
+                {
+                    // Posso remover com o indice do objeto
+                    produtos.RemoveAt(i);
+
+                    // Posso remover com o objeto desejado
+                    // produtos.Remove(produto);
+
+                    // Retorna verdadeiro porque o produto com o código desejado foi removido da lista
+                    return true;
+                }
+            }
+            // Retorna falso porque não foi encontrado produto com o código desejad, ou seja, nenhum elemento foi removido da lista
+            return false;
         }
         public List<Produto> ObterTodos()
         {
             return produtos;
         }
-        public Produto ObterPorCodigo()
+        public Produto ObterPorCodigo(int codigo)
         {
+            // Percorrer todos os elementos para encontrar o produto filtrndo por código
+            for (var i = 0; i < produtos.Count; i++)
+            {
+                // Obter o produto da lista de produtos em determinado indice
+                var produtoAtual = produtos[i];
+
+                // Verifica se o produto atual da lista percorrida é o desejado
+                if (produtoAtual.Codigo == codigo)
+                {
+                    // Retorna o produto encontrado
+                    return produtoAtual;
+                }
+            }
+
+            // Retorna o null, caso não encontre um produto com o código passado como parâmetro
             return null;
         }
     }
