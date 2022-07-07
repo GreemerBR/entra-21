9b8d0221ae994b3072c0e16c602c7f015ceee5ff
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-07-06 22:12:58 -0300

    Exercício 01 Foi iniciado o exercício 01

---
+++ Exercícios/SQL/Lista08.sql
@@ -0,0 +1,16 @@
+﻿-- Exercício 01
+CREATE TABLE pessoas(
+	nome VARCHAR(100),
+	cpf VARCHAR(14) PRIMARY KEY,
+	rg VARCHAR(12),
+	data_nascimento DATETIME2,
+	idade INTEGER
+	);
+
+INSERT INTO pessoas(nome, cpf, rg, data_nascimento, idade)
+	VALUES(
+	'Erick Bryan Enrico Vieira',
+	 '699.734.958-70',
+	 '90.745.276-0',
+	 '1989-05-28',
+	 29);
