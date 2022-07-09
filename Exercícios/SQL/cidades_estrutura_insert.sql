868347d192ac67bb4b06024aa69a18d2c1967bf6
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-07-08 21:34:21 -0300

    Lista 08.2 Finalizado os exercícios de SELECT do banco de dados de Cidades.

--- Exercícios/SQL/cidades_estrutura_insert.sql
+++ Exercícios/SQL/cidades_estrutura_insert.sql
@@ -5489,3 +5489,66 @@ INSERT INTO cidades VALUES
 ('TO', 'Tupiratins'),
 ('TO', 'Wanderlândia'),
 ('TO', 'Xambioá');
+
+----------------------------------------------------------------------------
+--LISTA 8.02
+-- 1. Selecione o estado e cidade.
+
+SELECT estado, cidade
+	FROM cidades;
+
+--2. Selecione as cidades que começam com a letra A.
+
+SELECT cidade
+	FROM cidades
+	WHERE cidade LIKE 'A%';
+
+--3. Selecione as cidades que contenham no ‘apar’ em qualquer parte do nome
+
+SELECT cidade
+	FROM cidades
+	WHERE cidade LIKE '%apar%';
+
+--4. Selecione as cidades que começam com a letra W.
+
+SELECT cidade
+	FROM cidades
+	WHERE cidade LIKE 'W%';
+
+--5. Selecione o estado e a cidade que a cidade termina com ‘tuba’ ordenando pelo estado em ordem decrescente.
+
+SELECT estado, cidade
+	FROM cidades
+	WHERE cidade LIKE '%tuba'
+	ORDER BY cidade DESC;
+
+--6. Selecione a cidade que o nome da cidade contenha mais que 15 caracteres, ordenando pelo quantidade de caracteres.
+
+SELECT cidade
+	FROM cidades
+	WHERE LEN(cidade) > 15
+	ORDER BY LEN(cidade) ASC;
+
+SELECT cidade
+	FROM cidades
+	WHERE LEN(cidade) > 15
+	ORDER BY LEN(cidade) DESC;
+
+--7. Contabilize a quantidade de cidades do estado de SC.
+
+SELECT COUNT(cidade) AS 'Quantidade de cidade de SC'
+	FROM cidades
+	WHERE estado LIKE 'SC';
+
+--8. Contabilize a quantidade de cidades do estado de SP.
+
+SELECT COUNT(cidade) AS 'Quantidade de cidades de SP'
+	FROM cidades
+	WHERE estado LIKE 'SP';
+
+--9. Selecione o nome da cidade com a quantidade de caracteres, quando o nome da cidade conter 10 caracteres.
+
+SELECT cidade,
+	   LEN(cidade) AS 'Quantidade de caracteres'
+	   FROM cidades
+	   WHERE LEN(cidade) = 10;
