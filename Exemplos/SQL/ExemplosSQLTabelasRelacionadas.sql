6ff77a4f2eb441616e8e1e534005a8a25a54c589
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-07-11 20:13:52 -0300

    SQL Início do exemplo de tabelas não relacionadas

---
+++ Exemplos/SQL/ExemplosSQLTabelasRelacionadas.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE pecas(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	tipo INTEGER,
+	preco_unitario DECIMAR(6,2));
+
+CREATE TABLE clientes(
+	id INTEGER PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(40),
+	cpf VARCHAR(14));
+
+CREATE TABLE enderecos(
+	id INTEGER PRIMARY KEY IDENTITY(1,1), -- PK
+	id_cliente INTEGER NOT NULL, --FK
+	cep VARCHAR(9) NOT NULL,
+	estado VARCHAR(30) NOT NULL,
+	cidade VARCHAR(30) NOT NULL,
+	bairro VARCHAR(30) NOT NULL,
+	logradouro VARCHAR(40) NOT NULL,
+	numero VARCHAR(10) NOT NULL,
+	descricao TEXT,
+
+	-- FOREIGN KEY (nome_coluna_tabela_atual) REFERENCES nome_tabela_pai(nome_coluna_pai_pk)
+	FOREIGN KEY (id_cliente) REFERENCES clientes(id));
